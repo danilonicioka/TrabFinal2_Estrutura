8efd395e91747a5691c0eb563507ca4f95621ade
Zuriki127 <76977246+Zuriki127@users.noreply.github.com>
2021-02-08 16:08:03 -0300

    Update and rename lista/Main.java to Simulacao.java

--- Simulacao.java
+++ Simulacao.java
@@ -1,18 +1,25 @@
 package lista;
 
-public class Main {
+public class Simulacao {
 
     public static void main(String[] args) {
         
+        /*
+         Programa que simula uma brincadeira infantil de roda.
+         Uma lista circular recebe elementos do tipo aluno,
+         cujo único atributo são seus nomes.
+         Dentro da lista, cada elemento guardará a posição do próximo.  
+         Para mais detalhes, confira o relatório.
+        */
         ListaCircular lc = new ListaCircular();
         
+        //Alunos hipotéticos
         Aluno a0 = new Aluno("Pedro");
         Aluno a1 = new Aluno("Tiago");
-        Aluno a2 = new Aluno("João");
+        Aluno a2 = new Aluno("Lucas");
         Aluno a3 = new Aluno("Golfo");
         Aluno a4 = new Aluno("Meier");
         Aluno a5 = new Aluno("Oscar");
-        
         
         lc.adiciona(a0);
         lc.adiciona(a1);
@@ -20,7 +27,6 @@ public class Main {
         lc.adiciona(a3);
         lc.adiciona(a4);
         lc.adiciona(a5);
-       
         
         lc.sequencia();
         
